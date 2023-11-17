804767445f9214dcbb90bbece704946c15381f54
Christy Nicholson <christy.nicholson@gmail.com>
2023-11-17 10:12:42 -0500

    Week 2 Homework

---
+++ Week2Homework.js
@@ -0,0 +1,21 @@
+const pizzaPlace = "Christy's House of Crust";
+let toppings = 10;
+console.log(pizzaPlace);
+console.log(typeof pizzaPlace);
+console.log(toppings);
+console.log(typeof toppings);
+console.log(
+  "Hello and welcome to " +
+    pizzaPlace +
+    " where you can have up to " +
+    toppings +
+    " toppings on your pie."
+);
+if (toppings <= 9) {
+  console.log("Quality, not quantity.");}
+    else {
+      console.log("A whole lot of pizza.")
+    }
+for (let i = 1; i <=10; i++) {
+  console.log(i);
+}
